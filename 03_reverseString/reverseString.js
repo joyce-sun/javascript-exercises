31a3dc36d62a4f47fc251f3cbfd4474b0f9041b2
Joyce <joyce-sun@outlook.com>
2023-03-03 23:48:12 -0500

    finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
-};
+const reverseString = function(word) {
+    let reverse = '';
+    let temp = word;
+    for (let i = 0; i < word.length; i++){
+        reverse += temp[temp.length - 1];
+        temp = temp.slice(0, -1);
+    }
+    return reverse;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
